dde72eff0a682f9fee7eaeca76671fe64e152f44
Hitesh Malhotra <hmalhotra_be19@thapar.edu>
2025-10-17 14:10:29 +0530

    bug: simplify super method calling

--- Chess Game/design.py
+++ Chess Game/design.py
@@ -168,7 +168,7 @@ class Board:
 
     def __new__(cls):
         if cls._instance is None:
-            cls._instance = super(Board, cls).__new__(cls)
+            cls._instance = super().__new__(cls)
             cls._instance._initialized = False
         return cls._instance
 
